18c7351fbc4f308098ac281c6a91764e6f61fe78
K1-mikaze <K1mikaze@proton.me>
2026-04-13 11:58:41 -0500

    The derivations already exist in nixpkgs

--- home-manager/system/desktop/gnome/custom/extensions.nix
+++ home-manager/system/desktop/gnome/custom/extensions.nix
@@ -5,45 +5,49 @@
   lib,
   ...
 }: let
-  dynamic-music-panel = pkgs.callPackage ../../../../../derivations/dynamic-music-pill {};
+  unstable-extensions = with pkgs-unstable.gnomeExtensions; [
+    dynamic-music-pill
+  ];
 in {
   options.system.desktop.gnome.custom.extensions.enable = lib.mkEnableOption "Enable Gnome Extensions";
 
   config = lib.mkIf config.system.desktop.gnome.custom.extensions.enable {
-    home.packages = with pkgs.gnomeExtensions; [
-      removable-drive-menu
-      gsconnect
-      touchpad-switcher
-      app-name-indicator
-      caffeine
-      just-perfection
-      top-panel-logo
-      paperwm
-      battery-health-charging
-      light-style
-      vertical-workspaces
-      dynamic-music-panel
-    ];
+    home.packages = with pkgs.gnomeExtensions;
+      [
+        removable-drive-menu
+        gsconnect
+        touchpad-switcher
+        app-name-indicator
+        caffeine
+        just-perfection
+        top-panel-logo
+        paperwm
+        battery-health-charging
+        light-style
+        vertical-workspaces
+      ]
+      ++ unstable-extensions;
 
     dconf = {
       enable = true;
       settings = {
         "org/gnome/shell" = {
           disable-user-extensions = false;
-          enabled-extensions = with pkgs.gnomeExtensions; [
-            removable-drive-menu.extensionUuid
-            touchpad-switcher.extensionUuid
-            app-name-indicator.extensionUuid
-            caffeine.extensionUuid
-            just-perfection.extensionUuid
-            gsconnect.extensionUuid
-            top-panel-logo.extensionUuid
-            paperwm.extensionUuid
-            battery-health-charging.extensionUuid
-            light-style.extensionUuid
-            vertical-workspaces.extensionUuid
-            dynamic-music-panel.extensionUuid
-          ];
+          enabled-extensions = with pkgs.gnomeExtensions;
+            [
+              removable-drive-menu.extensionUuid
+              touchpad-switcher.extensionUuid
+              app-name-indicator.extensionUuid
+              caffeine.extensionUuid
+              just-perfection.extensionUuid
+              gsconnect.extensionUuid
+              top-panel-logo.extensionUuid
+              paperwm.extensionUuid
+              battery-health-charging.extensionUuid
+              light-style.extensionUuid
+              vertical-workspaces.extensionUuid
+            ]
+            ++ map (ext: ext.extensionUuid) unstable-extensions;
         };
       };
     };
