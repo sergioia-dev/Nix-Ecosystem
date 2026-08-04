790678fd207a699cb046bd963b20352fc2bce625
sergioia-dev <sergioidarraga2110@gmail.com>
2026-08-04 08:57:13 -0500

    Update battery thershold configuration

--- nixos/module/system/desktop/niri/services/configuration.nix
+++ nixos/module/system/desktop/niri/services/configuration.nix
@@ -15,6 +15,20 @@
       nautilus
     ];
 
+    # Battery Threshold Control (noctalia plugin): give the user process write access to
+    # /sys/class/power_supply/BAT0/charge_control_end_threshold. Declarative equivalent of
+    # the plugin's setup_rules.sh (stock script can't run on NixOS: read-only /etc, no /bin).
+    users.groups.battery_ctl = { };
+
+    users.users.sia.extraGroups = [ "battery_ctl" ];
+
+    services.udev.extraRules = ''
+      # Battery Threshold Control - grants write access to charge_control_end_threshold for battery_ctl group
+      SUBSYSTEM=="power_supply", KERNEL=="BAT*", \
+          RUN+="${pkgs.coreutils}/bin/chgrp battery_ctl /sys$devpath/charge_control_end_threshold", \
+          RUN+="${pkgs.coreutils}/bin/chmod g+w /sys$devpath/charge_control_end_threshold"
+    '';
+
     programs.niri.enable = true;
     services.gvfs.enable = true;
 
