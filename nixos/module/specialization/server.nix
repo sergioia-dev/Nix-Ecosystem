3f736698b0edf56f0f929d9a485cd8b8a48295f9
sergioia-dev <sergioidarraga2110@gmail.com>
2026-08-10 14:45:23 -0500

    No power off or suspend

--- nixos/module/specialization/server.nix
+++ nixos/module/specialization/server.nix
@@ -35,6 +35,30 @@
           ];
         };
 
+        systemd = {
+          targets = {
+            sleep = {
+              enable = false;
+              unitConfig.DefaultDependencies = "no";
+            };
+
+            suspend = {
+              enable = false;
+              unitConfig.DefaultDependencies = "no";
+            };
+
+            hibernate = {
+              enable = false;
+              unitConfig.DefaultDependencies = "no";
+            };
+
+            "hybrid-sleep" = {
+              enable = false;
+              unitConfig.DefaultDependencies = "no";
+            };
+          };
+        };
+
         services.tlp = {
           enable = true;
           settings = {
